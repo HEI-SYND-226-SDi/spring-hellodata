e16c65de4acfd777f98cf0563fc5ad0ff3db2c5a
Michael Clausen <michael.clausen@hevs.ch>
2018-04-16 18:31:15 +0200

    Created project using start.spring.io

---
+++ src/main/java/ch/hevs/sdi/springhellodata/SpringHellodataApplication.java
@@ -0,0 +1,12 @@
+package ch.hevs.sdi.springhellodata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHellodataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHellodataApplication.class, args);
+    }
+}
